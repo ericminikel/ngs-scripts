26d7c1703664a12a0099cefbeee348d2d5be8a90
Eric Minikel <ericminikel@users.noreply.github.com>
2014-01-17 11:36:50 -0500

    Update runXHMM.bash

--- runXHMM.bash
+++ runXHMM.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # to run:
-# bsub -o /humgen/atgu1/fs03/eminikel/sandbox/runXHMMjoboutput.out "bash runXHMM.bash -v -o /humgen/atgu1/fs03/eminikel/sandbox/2"
+# bsub -o /humgen/atgu1/fs03/eminikel/sandbox/runXHMMjoboutput.out "bash runXHMM.bash -v -o /humgen/atgu1/fs03/eminikel/sandbox/2 -b /humgen/atgu1/fs03/eminikel/sandbox/bam.list"
 
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
@@ -89,7 +89,7 @@ xhmm --matrix -r $outdir/DATA.RD.txt \
     --excludeSamples $outdir/DATA.PCA_normalized.filtered.sample_zscores.RD.txt.filtered_samples.txt \
     -o $outdir/DATA.same_filtered.RD.txt
 
-echo "1e-08   6       70      -3      1       0       1       3       1" | cat > params.txt
+echo "1e-08   6       70      -3      1       0       1       3       1" | cat > $outdir/params.txt
 
 xhmm --discover -p $outdir/params.txt \
     -r $outdir/DATA.PCA_normalized.filtered.sample_zscores.RD.txt -R $outdir/DATA.same_filtered.RD.txt \
